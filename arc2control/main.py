b86887e422f4ac501ca3088726e81ba2e81821e9
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2021-08-17 00:54:01 +0100

    Implement module loading from standard directories
    To allow custom modules to be built `arc2control` will look into standard
    data directories for modules that qualify. A
    python package named `arc2emods` *MUST* be present for modules to be
    loaded from that location. Higher locality modules have higher priority
    (so `~/.local/share/arc2control/arc2emods` wins over
    `/usr/share/arc2control/arc2emods` when module name clash occurs)

--- arc2control/main.py
+++ arc2control/main.py
@@ -103,7 +103,7 @@ class ChannelMapper:
         return self._nwords * self._nbits
 
 
-def _discover_modules(path):
+def _discover_modules(path, base='arc2control.modules'):
 
     from pkgutil import iter_modules
     import importlib
@@ -118,17 +118,20 @@ def _discover_modules(path):
 
         loader = finder.find_module(name)
         try:
-            mod = importlib.import_module('arc2control.modules.%s' % name)
+            mod = importlib.import_module('%s.%s' % (base, name))
             mods[mod.MOD_NAME] = mod.ENTRY_POINT
-        except (ModuleNotFoundError, ImportError, KeyError, AttributeError):
+            print("Importing module:", mod)
+        except (ModuleNotFoundError, ImportError, KeyError, AttributeError) as exc:
             # either `MOD_NAME`/`ENTRY_POINT` are not defined, module
             # does not exist (for some reason) or module contains error
+            print(exc)
             continue
 
     return mods
 
 
 def main():
+    import sys
     realpath = os.path.dirname(os.path.realpath(__file__))
     mapfile = os.path.join(realpath, 'mappings', 'resarray32.toml')
 
@@ -140,13 +143,48 @@ def main():
 
     mapper = ChannelMapper(words, bits, wordarr, bitarr)
 
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName('arc2control')
+    graphics.initialise()
+
+    # Try to discover modules in QStandardPaths; locateAll will
+    # produce a list of standard data locations with decreasing locality
+    # (and therefore decreasing priority). Our data folder *MUST* contain
+    # a python package named `arc2emodules` to qualify
+    paths = QtCore.QStandardPaths.locateAll(\
+            QtCore.QStandardPaths.StandardLocation.AppDataLocation, \
+            'arc2emodules', \
+            QtCore.QStandardPaths.LocateOption.LocateDirectory)
+
+    # check all the paths returned from `locateAll`
+    for p in paths:
+        # check for an `__init__.py` file to find out if this
+        # folder is indeed a python package
+        if os.path.exists(os.path.join(p, '__init__.py')):
+            # success, add it to the path. Since we are traversing
+            # the paths from higher to lower priority, paths returned
+            # first will have higher priority if modules of the same
+            # name exist
+            sys.path.append(os.path.dirname(p))
+        else:
+            print("%s exists but doesn't look like a package" % p)
+
+    # discover built-in modules first
     from . import modules as basemodmod
     mods = _discover_modules(basemodmod.__path__)
 
-    app = QtWidgets.QApplication([])
-    app.setApplicationName('arc2control')
-    graphics.initialise()
-    wdg = App(mapper, modules=mods)
+    # try to discover external modules now
+    try:
+        # this will only fail if there are no `arc2emodules` packages
+        # found during the the loop above, there's nothing to do
+        import arc2emodules as baseemodmod
+        emods = _discover_modules(baseemodmod.__path__, 'arc2emodules')
+    except ModuleNotFoundError:
+        # no external modules
+        emods = {}
+
+    # load the app, merging all modules into a dict
+    wdg = App(mapper, modules={**mods, **emods})
     wdg.show()
     app.exec()
 
