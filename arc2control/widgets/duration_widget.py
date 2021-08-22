45f160d239b84b3bb0d3df9fb48518dffe018e84
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2021-08-23 00:00:14 +0100

    Add additional utility functions to DurationWidget

--- arc2control/widgets/duration_widget.py
+++ arc2control/widgets/duration_widget.py
@@ -12,8 +12,8 @@ class DurationWidget(QtWidgets.QWidget):
         self.baseValueSpinBox.setMinimumWidth(40)
 
         self.multiplierCombo = QtWidgets.QComboBox()
-        self.multiplierCombo.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum,\
-            QtWidgets.QSizePolicy.Policy.Preferred)
+        self.multiplierCombo.setSizePolicy(QtWidgets.QSizePolicy.Policy.Preferred,\
+            QtWidgets.QSizePolicy.Policy.Maximum)
         self.multiplierCombo.addItem('ns', 1.0e-9)
         self.multiplierCombo.addItem('μs', 1.0e-6)
         self.multiplierCombo.addItem('ms', 1.0e-3)
@@ -31,8 +31,45 @@ class DurationWidget(QtWidgets.QWidget):
         self.baseValueSpinBox.setObjectName('%s_baseValue' % what)
         self.multiplierCombo.setObjectName('%s_multiplier' % what)
 
+    def setDurations(self, durations):
+        self.baseValueSpinBox.clear()
+        self.multiplierCombo.clear()
+
+        for (lbl, mult) in durations:
+            self.multiplierCombo.addItem(lbl, mult)
+
+    def setCurrentMultiplierIndex(self, idx):
+        self.multiplierCombo.setCurrentIndex(idx)
+
+    def setCurrentMultiplier(self, mlt):
+        if isinstance(mlt, str):
+            check_labels = True
+        elif isinstance(mlt, (int, float)):
+            check_labels = False
+        else:
+            raise ValueError("Invalid multiplier; it must be a string or number")
+
+        for i in range (0, self.multiplierCombo.count()):
+            label = self.multiplierCombo.itemText(i)
+            mult = self.multiplierCombo.itemData(i)
+
+            if check_labels:
+                if label == mlt:
+                    self.multiplierCombo.setCurrentIndex(i)
+                    return
+            else:
+                if mult == mlt:
+                    self.multiplierCombo.setCurrentIndex(i)
+                    return
+
+        raise ValueError("Multiplier doesn't exist in widget")
+
     def getDuration(self):
         base = self.baseValueSpinBox.value()
         multiplier = self.multiplierCombo.currentData()
 
         return base * multiplier
+
+    def setDuration(self, value, mult):
+        self.baseValueSpinBox.setValue(value)
+        self.setCurrentMultiplier(mult)
