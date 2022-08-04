80eb5e10aa0cb8e7b39e5093b1fe4e93e3fe98f4
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2022-08-04 20:55:08 +0100

    Rework AppImage generation and add a build script

---
+++ appimage/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DIR=$(realpath $(dirname "$0")/..)
+
+pushd $DIR
+# pick up existing requirements
+cp requirements.txt appimage/requirements.txt
+# and also add the directory of arc2control
+echo "${DIR}" >> appimage/requirements.txt
+
+# build it with python-appimage
+python-appimage build app -l manylinux2014_x86_64 -p 3.10 -n arc2control appimage
+
+if [ -f appimage/requirements.txt ]; then
+  rm appimage/requirements.txt
+fi
+
+popd
