c102078f601a3869c8abd7156830fc42fcabf185
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2022-08-06 14:18:28 +0100

    Add a documentation upload script

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ArC Instruments Ltd'
 author = 'Spyros Stathopoulos'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0-a0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
